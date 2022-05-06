bf3da465f4979abead4a854c40ad6efc1f2f2c1b
Juan Pablo Morales <37224612+jpmorales1999@users.noreply.github.com>
2022-05-06 17:12:46 -0500

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div>
-         <p className="text-center text-light fst-italic" style={{fontWeight: 'bold', fontSize: '1rem', marginTop: '30px'}}>Copyright &copy; 2022. Todos los derechos reservados</p>
+         <p className="text-center text-light fst-italic" style={{fontWeight: 'bold', fontSize: '1rem', marginTop: '30px'}}>Todos los derechos reservados &copy; 2022</p>
     </div>
   )
 }
